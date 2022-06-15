a2d822f86a77a9317f2c0d57e93e429666f99bdd
SWWI <swwirin@gmail.com>
2022-06-15 21:28:38 +0500

    'new'

--- foods/urls.py
+++ foods/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'foods'
 urlpatterns = [
-    path('', views.FoodView.as_view()),
+    path('', views.index),
+    path('foods/', views.FoodView.as_view()),
+    path('menu/', views.menu),
     # path('test/', views.food),
     path('<int:pk>', views.FoodDetailView.as_view())
+
 ]
--- foods/views.py
+++ foods/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Food
 
+
+def index(request):
+    return render(request, "base.html", {})
+
+
+def menu(request):
+    return render(request, 'menu.html',{})
+
+
 class FoodView(View):
     def get(self, request):
         foods = Food.objects.all()
@@ -11,7 +20,7 @@ class FoodView(View):
 class FoodDetailView(View):
     def get(self, request, pk):
         food = Food.objects.get(id=pk)
-        return render(request, 'food.html', {"food":food})
+        return render(request, 'food.detail.html', {"food":food})
 
     # c2 = Food.objects.all()
     # context = {'obj_list': c2}
